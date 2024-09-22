1169fb26980308e6365b1d6ffc00cae5c88edd0d
rygellouv <quentinlouv@gmail.com>
2024-09-22 13:59:58 +0000

    Initial Commit

---
+++ Tanga/TangaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TangaApp.swift
+//  Tanga
+//
+//  Created by Rygel Louv on 22/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct TangaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TangaTests/TangaTests.swift
@@ -0,0 +1,16 @@
+//
+//  TangaTests.swift
+//  TangaTests
+//
+//  Created by Rygel Louv on 22/09/2024.
+//
+
+import Testing
+
+struct TangaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
